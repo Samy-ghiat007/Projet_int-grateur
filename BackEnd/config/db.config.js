fd8ed8ab85047316cc770ed1f55c05b97e0604b2
Mohamed Ali Bachar <alibachar27@gmail.com>
2024-06-06 16:18:10 -0400

    fixed tests, added cloud database

--- BackEnd/config/db.config.js
+++ BackEnd/config/db.config.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+    }
+);
 
 sequelize.authenticate()
     .then(() => {
